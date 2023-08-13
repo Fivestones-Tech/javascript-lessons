35aa6d4d501fe1dfc1d3bc7afd386bfbb3300926
Ogunaike Matthew Folawe <125424571+Fivestones-Tech@users.noreply.github.com>
2023-08-13 01:35:16 +0100

    javascript lessons

---
+++ lessons1/lessonin.js
@@ -0,0 +1,190 @@
+// comment can make code readable//
+
+//welcome to 30daysofjavacsript//
+
+/*
+comment can make code readable
+easy to use and informative
+*/
+
+// var name = 'matthew'
+
+// const lightOn = true
+// console.log(lightOn)
+
+// let inTheBag =
+// console.log(inTheBag);
+
+// console.log(typeof 'matthew')
+// console.log(typeof 5)
+// console.log(typeof true)
+// console.log(typeof undefined)
+
+// const firstName = 'matthew'
+// const lastName = 'ogunnaike'
+// let maritalStatus = 'engage'
+// var country = 'Nigeria'
+// let age = 28
+
+// console.log(firstName, lastName, maritalStatus, country, age)
+
+// const firstName = 'matthew', lastName = 'ogunnaike', maritalStatus = 'engage', country = 'Nigeria', age = 28
+// console.log(firstName, lastName, maritalStatus, country, age)
+
+// let myAge = 28
+// let yourAge = 23
+// console.log('I', 'am', myAge, 'years', 'old')
+// console.log('you', 'are', yourAge, 'years', 'old')
+
+
+// Day 2
+
+// let age = 26
+// let year = 2022
+// console.log(age == year)
+
+// let tundeGotHereBy = 12
+// let sadeGotHereby = 12
+// console.log( tundeGotHereBy == sadeGotHereby)
+
+// let num = [5, 2, 3]
+// num[1] = 50
+
+// console.log(num)
+
+// let nums = [1, 2, 3]
+// let nums = [1, 2, 3]
+
+// console.log(nums == nums)
+
+
+// non-primitive data type
+// this array object is mutable
+/*
+let number = 20
+let object = number
+
+console.log(number == object)
+*/
+// let age = 20
+// const gravity = 9.34
+// let mass = 46
+// const PI = 3.33
+
+// const boilingPoint = 100
+// const bodyTemp = 27
+
+// console.log(age, gravity, mass, PI, boilingPoint, bodyTemp)
+
+// const PI = 3.
+
+// let tecgGuys = 'follow'
+
+// let techGirls = 'me'
+
+// console.log('kindly', tecgGuys, techGirls,) 
+
+// what will be the output?
+
+// const PI = Math.PI
+
+// console.log(PI)
+// console.log(Math.round(PI))
+
+// console.log(Math.round(9.53))
+// console.log(Math.floor(PI))
+// console.log(Math.floor(2.9))
+
+// console.log(Math.ceil(PI))
+// console.log(Math.ceil(9.8))
+
+// console.log(Math.min(-3, -4, -1, 6))
+
+// const randNum = Math.random
+
+// let fullName = 'taye kehinde'
+// console.log(fullName)
+
+
+// let fullName = 'matric'
+// let firstName = 'matthew'
+// let lastName = 'ben'
+// let country = 'nigeria'
+// let age = 25
+
+// // console.log(fullName +""+ lastName)
+// let fullName =firstName + space + lastName
+// let personInfoOne = fullName + '. I am ' + age + '. I live in ' + country;
+
+// let lightOn = true
+// let lightOff = lightOn
+
+// console.log(lightOn == lightOff)
+
+
+// let randomNum = Math.random()
+// let numBtnZeroAndTen = randomNum * 11
+
+// console.log(numBtnZeroAndTen) 
+
+/*
+let space = ' ' 
+let firstName = 'matthew'
+let lastName = 'Yetayeh'
+let country = 'Finland'
+let city = 'Helsinki'
+let language = 'JavaScript'
+let job = 'teacher'
+let quote = "The saying,'Seeing is Believing' is not correct in 2020."
+let quotWithBackTick = `The saying,'Seeing is Believing' is not correct in 2020.`
+
+
+let fullName = firstName +  space + lastName;
+console.log(fullName)
+\*/
+
+// console.log(`The sum of 2 and 3 is 5`)
+// let a = 2
+// let b = 3
+// console.log(`The sum of ${a} and ${b} is ${a + b}`) 
+// console.log(the sum of 'a' and 'b' is 5)
+
+// let myName = 'stephen'
+// console.log(myName.length)
+
+// console.log(myName.toUpperCase())
+// console.log(myName.substr(2,5))
+// console.log(myName.substring(3,7))
+
+// let string = '30 Days Of JavaScript'
+
+// console.log(string.split())     // Changes to an array -> ["30 Days Of JavaScript"]
+// console.log(string.split(' '))
+// console.log(string.replace('JavaScript', 'python'))
+
+// let randomNum = Math.random()
+// let numZeroAndTen = randomNum * 11
+
+// console.log(numZeroAndTen)
+
+// let randomNum = Math.random()
+// console.log(randomNum)
+
+// console.log(`The sum of 2 and 3 is 5`) 
+
+// let a = 3
+// let c = 7
+// console.log(`combine ${a} and ${c} so that it can give you 37`)
+
+// let firstName = 'matthew'
+// let lastName = 'mathias'
+
+// console.log(`${firstName} is my name and my surname is ${lastName}`)
+
+let challenge = '30 days of javscript'
+
+console.log(challenge.toUpperCase())
+
+
+
+
